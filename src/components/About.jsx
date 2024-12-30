318af7c919519010a7b998ff550c819bf6743a1c
brajeshsin <brajeshsingh2911@gmail.com>
2024-12-31 00:11:09 +0530

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -29,8 +29,8 @@ const About = () => {
   return (
     <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
-        <p className="font-general text-sm uppercase md:text-[10px]">
-          Welcome to Zentry
+        <p className="font-general text-3xl uppercase md:text-[30px]">
+          Welcome to Brajesh Gaming
         </p>
 
         <AnimatedTitle
