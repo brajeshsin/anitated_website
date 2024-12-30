a2422e2e39facb79f271eecb263ee8ea73875a4f
brajeshsin <brajeshsingh2911@gmail.com>
2024-12-30 23:58:35 +0530

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -148,7 +148,8 @@ const Hero = () => {
           <div className="mt-24 px-5 sm:px-10">
             <h1 className="special-font hero-heading text-blue-100">
               {/* redefi<b>n</b>e */}
-              braje<b>s</b>h
+              {/* braje<b>s</b>h */}
+              ank<b>i</b>t
             </h1>
 
             <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
